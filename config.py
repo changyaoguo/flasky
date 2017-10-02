3196f74410f777a8146e40292b86147111052b32
changyaoguo <1185302919@qq.com>
2017-10-02 22:07:05 +0800

    cyg

--- config.py
+++ config.py
@@ -6,14 +6,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = '1185302919@qq.com'#os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'spaiwibsxywphfbj'#os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER =  '1185302919@qq.com'#'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN =  '1185302919@qq.com'#os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 10
+    FLASKY_FOLLOWERS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
